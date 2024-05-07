9ca9d16af0cb0489b9790e669901c1207d5472c1
kim <malcomchege0582@gmail.com>
2024-05-07 11:58:56 +0300

    flatlist

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import exercises from './assets/data/exercises.json'
 
 export default function App() {
-  const exercise = exercises[0]
+
  console.log(exercises[0])
   return (
     <View style={styles.container}>
-      <View style={styles.exerciseContainer}>
-      <Text style={styles.exerciseName}>{exercise.name}</Text>
-      <Text style={styles.exerciseSubtitle}>Muscle: {exercise.muscle.toLocaleUpperCase()} | Equipment: {exercise.equipment.toLocaleUpperCase()}</Text>
-      </View>
+      <FlatList 
+      data={exercises}
+      renderItem={({item,index})=>{
+        return(
+          <View style={styles.exerciseContainer}>
+          <Text style={styles.exerciseName}>{item.name}</Text>
+          <Text style={styles.exerciseSubtitle}>Muscle: {item.muscle.toLocaleUpperCase()} | Equipment: {item.equipment.toLocaleUpperCase()}</Text>
+          </View>
+        )
+      }}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
